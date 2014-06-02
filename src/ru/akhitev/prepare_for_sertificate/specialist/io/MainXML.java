cab56f1a76d5c582ab5599341c82ff7b123f9f2d
Alex Khitev <hitev@tsn.spb.ru>
2014-06-02 12:58:26 +0400

    xml

--- src/ru/akhitev/prepare_for_sertificate/specialist/io/MainXML.java
+++ src/ru/akhitev/prepare_for_sertificate/specialist/io/MainXML.java
@@ -1,16 +1,27 @@
 package ru.akhitev.prepare_for_sertificate.specialist.io;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by hitev on 02.06.14.
  */
 public class MainXML {
-    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+    public static void main(String[] args) throws IOException, XMLStreamException, ParserConfigurationException, SAXException {
+        // С помощью XMLStreamReader/XMLStreamWriter
         XMLOutputFactory factory = XMLOutputFactory.newFactory(); // Получили фабрику, вызвав статический метод
         XMLStreamWriter writer = factory.createXMLStreamWriter(
                 new FileOutputStream("test.xml"), "utf-8"
@@ -21,5 +32,22 @@ public class MainXML {
         writer.writeEndElement();
         writer.writeEndDocument();
         writer.close();
+
+        // Через объектную модель
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(
+                new FileInputStream("books.xml")
+        );
+        NodeList nodeList = document.getElementsByTagName("book");
+        for(int i = 0; i< nodeList.getLength(); i++){
+            Node node = nodeList.item(i);
+            System.out.println(
+                    node.getAttributes().getNamedItem("price").getNodeValue()
+            );
+            System.out.println(
+                    node.getFirstChild().getNodeValue().trim()
+            );
+        }
     }
 }
